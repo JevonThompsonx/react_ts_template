cf38fb5d21491cc90e69fd6dd9a92c3bfa207e74
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2025-02-25 16:48:47 -0800

    Page wasn't centered before

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,15 @@
-import { Link } from "react-router-dom";
+import { Nav } from "../components"
 export default function NotFound() {
 
-  return (<div className="flex flex-col justify-evenly items-center space-y-2">
-    <h1>Sorry page not found</h1>
-    <Link to='/'> Home page </Link>
-    <Link to='/about'> About page </Link>
-  </div>)
+  return (
+
+    <>
+
+      <Nav />
+      <div className="flex flex-col justify-evenly items-center space-y-2 h-screen w-screen">
+        <h1>Sorry page not found</h1>
+
+      </div>
+    </>
+  )
 }
